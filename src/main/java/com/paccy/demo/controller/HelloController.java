7632c975d923b86c9ddee8c58e5b116567a8204c
PaccyC <abayisengaaimepacifique.com>
2024-01-04 09:48:05 +0200

    Changed application.yaml

--- src/main/java/com/paccy/demo/controller/HelloController.java
+++ src/main/java/com/paccy/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.paccy.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     //    @RequestMapping(value = "/",method = RequestMethod.GET)
+    @Value("${welcome.message}")
+    private String welcomeMessage;
     @GetMapping("/")
     public String helloWorld(){
-        return "Paccy is welcoming you to springboot app development";
+        return welcomeMessage;
     }
 }
